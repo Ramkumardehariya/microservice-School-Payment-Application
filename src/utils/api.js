2d0acaeee133582095cba4bfe396f16c1551b11d
Ramkumar Dehariya <ramkumardehariya26@gmail.com>
2025-09-18 14:12:54 +0530

    updaate url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 // src/utils/api.js
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
