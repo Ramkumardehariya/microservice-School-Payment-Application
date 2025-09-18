da68b8d11880c9ba4782527500a81b950db43a2d
Ramkumar Dehariya <ramkumardehariya26@gmail.com>
2025-09-18 09:09:15 +0530

    update the index.js backend file

--- server/index.js
+++ server/index.js
@@ -34,6 +34,13 @@ app.use('/api/v1/webhook', webhookRoutes);
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success:true,
+    message: "Server is running "
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
