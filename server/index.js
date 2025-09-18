2534393ca8d3b8cbcbc84d7355de9bdb9a3a0919
Ramkumar Dehariya <ramkumardehariya26@gmail.com>
2025-09-18 13:44:06 +0530

    update cors

--- server/index.js
+++ server/index.js
@@ -19,7 +19,23 @@ app.use(cookieParser());
 
 dbConnect();
 // CORS middleware
-app.use(cors({ origin: "*" }));
+const allowedOrigins = [
+  "http://localhost:3000", // Development
+  "https://microservice-school-paym-git-f26022-ramkumar-dehariyas-projects.vercel.app" // Production
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 
 // Routes
